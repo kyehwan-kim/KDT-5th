b32d4ea797045586bf945b4427b48253b059a02d
kyehwan <clon0306@gmail.com>
2023-02-10 10:49:40 +0900

    operator, if

---
+++ 03.JS/2.10/club.js
@@ -0,0 +1,37 @@
+let isAdult = true;
+let vip = true;
+let isDrunken = false;
+let money = true;
+
+//이건 내답
+if (isAdult || "vip") {
+  console.log("통과");
+} else {
+  console.log("돌아가");
+}
+
+if (isAdult && "vip" === isDrunken) {
+  console.log("통과");
+} else {
+  console.log("돌아가");
+}
+
+if ((!isAdult && !"vip" && isDrunken) || money) {
+  console.log("통과");
+} else {
+  console.log("돌아가");
+}
+
+//이건 강사님 답
+const 김계환 = {
+  성인인가: true,
+  VIP: false,
+  취했는가: true,
+  돈: false,
+};
+
+if (((김계환.성인인가 || 김계환.VIP) && 김계환.취했는가) || 김계환.돈) {
+  console.log("통과");
+} else {
+  console.log("돌아가");
+}
