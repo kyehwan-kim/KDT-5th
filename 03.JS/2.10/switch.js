fb97213d2b3399919d00da47f91ffe03150335f6
kyehwan <clon0306@gmail.com>
2023-02-10 11:40:45 +0900

    switch, ternary, club, today

---
+++ 03.JS/2.10/switch.js
@@ -0,0 +1,18 @@
+//SWITCH
+
+let gender = "M";
+
+switch (gender) {
+  case "F":
+    console.log("여성 입니다");
+    break;
+  case "M":
+    console.log("남성 입니다");
+    break;
+  default:
+    console.log("외계인 입니다");
+    break;
+}
+
+//SWITCH-2
+console.log(new Date().getDay());
---
+++ 03.JS/2.10/ternary.js
@@ -0,0 +1,12 @@
+//if문
+
+let name = "김계환";
+
+if (name === "김계환") {
+  console.log("맞았습니다");
+} else {
+  console.log("틀렸습니다");
+}
+
+//3항 연산자
+name === "김계환" ? console.log("맞았습니다") : console.log("틀렸습니다");
