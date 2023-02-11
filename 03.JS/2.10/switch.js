25b61964fe629645e6b9922753d057e04429326c
kyehwan <clon0306@gmail.com>
2023-02-11 19:54:12 +0900

    ㅤ

--- 03.JS/2.10/switch.js
+++ 03.JS/2.10/switch.js
@@ -15,4 +15,69 @@ switch (gender) {
 }
 
 //SWITCH-2
-console.log(new Date().getDay());
+const fingersOut = 1;
+
+switch (fingersOut) {
+  // 순서 상관없음
+  case 2:
+    console.log("가위");
+    break;
+  case 0:
+    console.log("바위");
+    break;
+  case 5:
+    console.log("보");
+    break;
+  default:
+    console.log("무효");
+}
+
+const direction = "east";
+let directionKor;
+
+switch (direction) {
+  case "north":
+    directionKor = "북";
+    break;
+  case "south":
+    directionKor = "남";
+    break;
+  case "east":
+    directionKor = "동";
+    break;
+  case "west":
+    directionKor = "서";
+    break;
+  default:
+    directionKor = "무효";
+}
+
+console.log(directionKor);
+
+let day;
+
+switch (new Date().getDay()) {
+  case 0:
+    day = "일요일";
+    break;
+  case 1:
+    day = "월요일";
+    break;
+  case 2:
+    day = "화요일";
+    break;
+  case 3:
+    day = "수요일";
+    break;
+  case 4:
+    day = "목요일";
+    break;
+  case 5:
+    day = "금요일";
+    break;
+  case 6:
+    day = "토요일";
+    break;
+}
+
+console.log(day);
--- 03.JS/2.10/today.js
+++ 03.JS/2.10/today.js
@@ -1,18 +1,38 @@
+// let day = new Date().getDay();
+
+// if (day === 0) {
+//   alert("일요일");
+// } else if (day === 1) {
+//   alert("월요일");
+// } else if (day === 2) {
+//   alert("화요일");
+// } else if (day === 3) {
+//   alert("수요일");
+// } else if (day === 4) {
+//   alert("목요일");
+// } else if (day === 5) {
+//   alert("금요일");
+// } else if (day === 6) {
+//   alert("토요일");
+// }
+// console.log(day);
+
 let day = new Date().getDay();
 
 if (day === 0) {
-  alert("일요일");
+  alert("일요일 입니다.");
 } else if (day === 1) {
-  alert("월요일");
+  alert("월요일 입니다.");
 } else if (day === 2) {
-  alert("화요일");
+  alert("화요일 입니다.");
 } else if (day === 3) {
-  alert("수요일");
+  alert("수요일 입니다.");
 } else if (day === 4) {
-  alert("목요일");
+  alert("목요일 입니다.");
 } else if (day === 5) {
-  alert("금요일");
+  alert("금요일 입니다.");
 } else if (day === 6) {
-  alert("토요일");
+  alert("토요일 입니다.");
 }
+
 console.log(day);
