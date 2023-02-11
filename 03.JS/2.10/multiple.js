7b0b172625f6462c8c54f0a9d8e1f5f0a22a4be0
kyehwan <clon0306@gmail.com>
2023-02-11 23:38:20 +0900

    js

--- 03.JS/2.10/multiple.js
+++ 03.JS/2.10/multiple.js
@@ -8,8 +8,18 @@
 
 // console.log(sum);
 
-for (let i = 2; i < 10; i++) {
-  for (let j = 1; j < 10; j++) {
-    console.log(`${i} x ${j} = ${i * j}`);
+// for (let i = 2; i < 10; i++) {
+//   for (let j = 1; j < 10; j++) {
+//     console.log(`${i} x ${j} = ${i * j}`);
+//   }
+// }
+
+let sum = 0;
+
+for (let i = 0; i < 101; i++) {
+  if (i % 2 === 0 || i % 5 === 0) {
+    sum += i;
   }
 }
+
+console.log(sum);
