54fced4cd66aed55578d17308989d6951668f717
kyehwan <clon0306@gmail.com>
2023-02-13 13:15:33 +0900

    JS:object

--- 02_1_object_this.js
+++ 02_1_object_this.js
@@ -1,22 +1,59 @@
-const user = {
-  name: "김계환",
-  sayHello: function () {
-    console.log(`안녕하세요, ${this.name}님`);
-  },
+// const user = {
+//   name: "김계환",
+//   sayHello: function () {
+//     console.log(`안녕하세요, ${this.name}님`);
+//   },
+// };
+
+// user.sayHello();
+
+// function sayHello() {
+//   console.log(`Hello, I'm ${this.name}`);
+// }
+
+// const boy = {
+//   name: "Mike",
+//   sayHello,
+// };
+
+// const girl = {
+//   name: "Jane",
+//   sayHello,
+// };
+
+// boy.sayHello();
+// girl.sayHello();
+
+//실습-this
+
+// const pororo = {
+//   name: "뽀로로",
+//   height: 110,
+//   showName() {
+//     console.log(`해당 객체의 이름은 ${this.name} 입니다`);
+//   },
+//   showHeight,
+// };
+
+// function showHeight() {
+//   console.log(`해당 객체의 이름은 ${this.height} 입니다`);
+// }
+
+// pororo.showName();
+// pororo.showHeight();
+
+//화살표 함수와 this
+let sayHello = () => {
+  console.log(`Hello, I'm ${this.name}`);
+  console.log(this);
 };
 
-user.sayHello();
-
-function sayHello() {
-  console.log(`Hello, I'm ${this.name}`);
-}
-
-const boy = {
+let boy = {
   name: "Mike",
   sayHello,
 };
 
-const girl = {
+let girl = {
   name: "Jane",
   sayHello,
 };
