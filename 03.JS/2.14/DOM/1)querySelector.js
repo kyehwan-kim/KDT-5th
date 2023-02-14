22231658d8b768e88dfef9b5a3e458b68e9d5b0b
kyehwan <clon0306@gmail.com>
2023-02-14 11:08:58 +0900

    JS:Dom

---
+++ 03.JS/2.14/DOM/1)querySelector.js
@@ -0,0 +1,11 @@
+let boxEl = document.querySelector(".box");
+console.log(boxEl);
+
+// let boxEl2 = document.querySelector("#box2");
+// console.log(boxEl2);
+
+let listEl = document.querySelector("ul>li:nth-child(2)");
+console.log(listEl);
+
+let thirdBoxEl = document.getElementById("third");
+console.log(thirdBoxEl);
